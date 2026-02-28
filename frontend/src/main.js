377e5117673947ae5fc889648df10af46d2f413a
Nanfahloic <yayzoy@gmail.com>
2026-02-28 17:25:16 -0500

    postjs

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,15 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import posthog from 'posthog-js'
+
+// Initialisation de PostHog mission 16.1
+posthog.init('phc_UW265ETldt4KJz27sBt5eLmsY1rfNcWzClWYWADGRHL', {
+    api_host: 'https://us.i.posthog.com',
+    person_profiles: 'always', // Nécessaire pour les nouvelles versions de PostHog
+    capture_pageview: true,
+    persistence: 'localStorage'
+})
 
 const app = createApp(App)
 app.use(router)
